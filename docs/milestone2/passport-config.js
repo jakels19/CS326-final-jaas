f6dbeb2c31fc2c73ff6a715a6fec04f3c1bfe7c0
jakels19 <jake205205@gmail.com>
2022-04-13 15:11:46 -0400

    passport/login authentication

---
+++ docs/milestone2/passport-config.js
@@ -0,0 +1,30 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByUsername, getUserByID){
+    const authenticateUser = async (username, password, done) => {
+        const user = getUserByUsername(username);
+        if (user == null){
+            return done(null, false, {message: 'No user exists'})
+        }
+
+    
+        try{
+            if (await bcrypt.compare(password, user.password)){
+                return done(null, user);
+            } else{
+                return done(null, false, {message: 'Password Incorrect'});
+            }
+        }catch (e){
+            return done(e);
+        }
+    }
+    passport.use(new localStrategy({usernameField: 'loginUsername', passwordField: 'loginPassword'},
+    authenticateUser))
+    passport.serializeUser((user,done) => done(null, user.id))
+    passport.deserializeUser((id,done) => {
+        return done(null, getUserByID(id))
+    })
+}
+
+module.exports = initialize
--- docs/milestone2/server.js
+++ docs/milestone2/server.js
@@ -1,24 +1,51 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require("express");
 const {faker} = require("@faker-js/faker");
 const app = express();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.set('view-engine', 'ejs');
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
+const initializePassport = require('./passport-config');
+
+initializePassport(
+    passport,
+    userName => users.find(user => user.userName === userName),
+    id => users.find(user => user.id === id)
+);
 
 let randomUsername = faker.internet.userName(); // random userName global
 let randomPassword = faker.internet.password(); // random password global
-/*app.post('/login', (req,res) => {
-// send fake user data to database
-res.render('login.html')
-res.send(`Username: ${randomUsername} Password: ${randomPassword}`); //responds by showing username and password
-});*/
+const users = [];
 
-app.get('/login', (req,res) =>{
-res.sendFile('.\public\login.html');
-//res.render('login.html')
-//res.send(`Username: ${randomUsername} Password: ${randomPassword}`); //responds by showing username and password
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
+    failureFlash: true
+
+}));
+
+app.get('/login', checkNotAuthenticated, (req,res) =>{
+res.render('login.ejs');
 });
 
 
@@ -26,7 +53,7 @@ app.get('/dashboard', (req,res) =>{
 
 });
 
-app.get('/signUp', (req,res) =>{
+app.get('/signUp', checkNotAuthenticated, (req,res) =>{
 
 });
 
@@ -38,7 +65,7 @@ app.get('/addIncome', (req,res) =>{
 
 });
 
-app.post('/signUp', (req,res) => {
+app.post('/signUp', checkNotAuthenticated, (req,res) => {
 // sends fake user data to db
 });
 
@@ -50,4 +77,23 @@ app.post('/addIncome', (req,res) => {
 
 });
 
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/login');
+})
+
+function checkAuthenticated(req, res, next){ //used for index
+    if (req.isAuthenticated()){
+        return next();
+    }
+
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/');
+    }
+    next();
+}
 app.listen(3000)
