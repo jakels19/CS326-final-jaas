219b3f7eb7241a40a7255f91af410fc693c0ac46
jakels19 <jake205205@gmail.com>
2022-04-19 19:50:21 -0400

    main empty files

---
+++ docs/milestone2/public/js/main.js
@@ -0,0 +1,27 @@
+async function createExpense(){
+    
+}
+
+async function createIncome(){
+
+}
+
+async function read(){
+
+}
+
+async function updateIncome(){
+
+}
+
+async function updateExpense(){
+
+}
+
+async function deleteIncome(){
+
+}
+
+async function deleteExpense(){
+
+}
